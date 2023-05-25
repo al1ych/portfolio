ef16402abd30114dd3b9040e27e0a448df676c71
al1ych <decter49pro@gmail.com>
2023-05-26 00:03:08 +0300

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+// boilerplate
+
+// import express
+const express = require('express');
+
+
+// create express app
+const app = express();
+
+// set port
+const port = 8000;
+
+// static files
+app.use(express.static(__dirname + '/public'));
+
+// listen on port 8000
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
---
+++ public/js/script.js
@@ -0,0 +1,33 @@
+window.onload = function () {
+    // 1. Создаем карту анимаций, которая соответствует именам элементов
+    const animationMap = {
+        'tim': 'img-tim-animation',
+        'hola-soy-text': 'hola-soy-text-animation',
+        'tim-text': 'tim-text-animation',
+        'hola-frontend': 'hola-frontend-animation',
+        'frontend-dev-text': 'frontend-dev-text-animation'
+    };
+
+    // 2. Получаем все элементы, которые мы хотим анимировать
+    const elements = [];
+    for (let id in animationMap) {
+        const element = document.getElementById(id);
+        const top = element.getBoundingClientRect().top + window.pageYOffset;
+        elements.push({element, top, animationClass: animationMap[id]});
+    }
+
+    // 3. Слушаем событие скролла
+    window.addEventListener('scroll', () => {
+        const scrollPosition = window.pageYOffset + window.innerHeight; // текущая позиция скролла + высота видимой области экрана
+
+        elements.forEach(({element, top, animationClass}) => {
+            if (scrollPosition >= top) {
+                // Если текущая позиция скролла больше или равна позиции элемента, добавляем класс анимации
+                element.classList.add(animationClass);
+            } else {
+                // В противном случае, удаляем класс анимации
+                element.classList.remove(animationClass);
+            }
+        });
+    });
+}
