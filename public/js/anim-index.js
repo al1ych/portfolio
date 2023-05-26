b4c6206224dfb0e609ceba15e4dd354cef04fade
al1ych <decter49pro@gmail.com>
2023-05-26 16:50:37 +0300

    updgrade animations

--- public/js/anim-index.js
+++ public/js/anim-index.js
@@ -2,18 +2,17 @@ const elements = [];
 
 window.onload = function () {
     const animationGroup = {
-        // one element triggers a group of animations
         'tim': ['tim', 'hola-soy-text', 'tim-text', 'hola-frontend', 'frontend-dev-text'],
     };
 
     for (let id in animationGroup) {
         const element = document.getElementById(id);
         const top = element.getBoundingClientRect().top + window.pageYOffset;
-        for (let animationClass of animationGroup[id]) {
+        for (let animatedElement of animationGroup[id]) {
             elements.push({
-                element: document.getElementById(animationClass),
+                element: document.getElementById(animatedElement),
                 top,
-                animationClass: `${animationClass}-animation`
+                animationClass: `${animatedElement}-animation`
             });
         }
     }
@@ -25,6 +24,7 @@ window.onload = function () {
             if (scrollPosition >= top) {
                 // Если текущая позиция скролла больше или равна позиции элемента, добавляем класс анимации
                 element.classList.add(animationClass);
+                setTimeout(curtainFall, 5000);
             } else {
                 // В противном случае, удаляем класс анимации
                 // element.classList.remove(animationClass);
@@ -32,50 +32,22 @@ window.onload = function () {
         });
     });
 
-    // get absolute Y bottom of div with id 'tim' in the document
-    const tim = document.getElementById('tim');
-    const timBottom = tim.getBoundingClientRect().bottom + window.pageYOffset;
-    console.log("timBottom", timBottom)
-    let reachedPageBottom = false;
-    let animationFinished = false;
-    let wheelListener;
-    window.addEventListener('scroll', () => {
-        if (animationFinished) {
-            return;
-        }
-        const scrollPosition = window.pageYOffset + window.innerHeight;
-        if (scrollPosition >= timBottom - 1) {
-            console.log("timBottom reached")
-            if (!reachedPageBottom) {
-                setTimeout(() => {
-                    reachedPageBottom = true;
-                    wheelListener = window.addEventListener('wheel', function (event) {
-                        if (event.deltaY < 0) {
-                            // console.log('Прокрутка вверх', event.deltaY);
-                            this.document.getElementById('title').style.opacity = 1;
-                            this.document.getElementById('hola').style.opacity = 1;
-                            reachedPageBottom = false;
-                            window.removeEventListener('wheel', wheelListener);
-                        } else if (event.deltaY > 0) {
-                            // console.log('Прокрутка вниз', event.deltaY);
-                            if (reachedPageBottom) {
-                                const oldOpacity = this.document.getElementById('hola').style.opacity || 1;
-                                const newOpacity = Math.max(0, oldOpacity - event.deltaY / 3000);
-                                this.document.getElementById('title').style.opacity = newOpacity;
-                                this.document.getElementById('hola').style.opacity = newOpacity;
-                                console.log("newOpacity", newOpacity)
-                                if (newOpacity === 0) {
-                                    window.removeEventListener('wheel', wheelListener);
-                                    this.location.href = '/about-and-beyond';
-                                    animationFinished = true;
-                                }
-                            }
-                        }
-                    });
-                }, 2000);
+    function curtainFall() {
+        document.getElementById('tim').classList.add('tim-animation-rev');
+
+        // todo
+        // 1 пролистнуть до конца экрана
+        // 2 заблокировать скроллинг
+
+        for (let i in animationGroup) {
+            for (let id of animationGroup[i]) {
+                console.log(id);
+                document.getElementById(id).classList.add(`${id}-animation-rev`);
             }
         }
-    });
 
-
+        setTimeout(() => {
+            location.href = '/about-and-beyond';
+        }, 1400); // animation length
+    }
 }
