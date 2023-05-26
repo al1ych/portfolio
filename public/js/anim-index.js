92f6ab8693115307c344d25ee6762439586ecdf8
al1ych <decter49pro@gmail.com>
2023-05-26 19:45:46 +0300

    rework animations in index

--- public/js/anim-index.js
+++ public/js/anim-index.js
@@ -35,7 +35,7 @@ window.onload = function () {
                     // block scroll
                     document.body.style.overflow = 'hidden';
                 }
-                setTimeout(curtainFall, 5000);
+                setTimeout(curtainFall, 3000);
             } else {
                 // В противном случае, удаляем класс анимации
                 // element.classList.remove(animationClass);
